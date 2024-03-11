c6926ffa4b1e253ab9e151ebef050b8f3de28e0e
Mate Kukri <km@mkukri.xyz>
2024-03-11 22:29:48 +0000

    H110M-A/DP Shadow SIO PNP accesses
    Change-Id: I4950cc0a31552e4b5424e492aac5eda9c1a12210

--- SerialICE/simba/mainboard/asus_h110m_adp.lua
+++ SerialICE/simba/mainboard/asus_h110m_adp.lua
@@ -1,6 +1,13 @@
 -- SPDX-License-Identifier: GPL-2.0-or-later
 
 function mainboard_io_pre(f, action)
+	if action.addr == 0x2e or action.addr == 0x2f then
+		return true
+	end
+	if action.addr == 0x1808 then
+		action.value = 1
+		return false
+	end
 	-- Check if the last 16-bit POST code tells us the RAM controller is ready
 	if action.write and action.addr == 0x80 and action.data == 0xdd5d then
 		if not ram_enabled() then
@@ -25,6 +32,8 @@ function do_mainboard_setup()
 	enable_hook(cpumsr_hooks, filter_intel_microcode)
 	enable_hook(cpuid_hooks, filter_multiprocessor)
 	enable_hook(cpuid_hooks, filter_feature_smx)
+
+	enable_hook(io_hooks, filter_mainboard_io)
 
 	new_car_region(0xfef00000, 0x80000)
 end
